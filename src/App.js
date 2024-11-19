f0562d59b523f8973caf2d59326ed9d724d174e9
mcgeeml <42824064+mcgeeml@users.noreply.github.com>
2024-11-19 01:52:45 -0600

    fixed the routes of the pages

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/summertime-volleyball">
         <Routes>
         <Route path="/" element={<Home/>}/>
           <Route path="/Video" element={<Video/>}/>
