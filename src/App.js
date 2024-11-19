c4e14e9039156c2798d9a2dac507413009f36021
mcgeeml <42824064+mcgeeml@users.noreply.github.com>
2024-11-19 15:05:02 -0600

    resized the app for smaller displays

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { useState, useEffect } from "react";
 import Video from "./pages/videoPage/Tape"
 import Home from "./pages/homePage/Home"
 import ContactPage from "./pages/contactPage/Contact"
@@ -6,11 +7,28 @@ import NoPage from "./pages/ErrorPage/NoPage"
 import './App.css';
 
 function App() {
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // Update the window width on resize
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Clean up event listener on unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
       <BrowserRouter basename="/summertime-volleyball">
         <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={windowWidth <= 450 ? <Video /> : <Home />} />
           <Route path="/Video" element={<Video/>}/>
           <Route path="/Contact" element={<ContactPage/>}/>
           <Route path="*" element={<NoPage/>}/>
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,56 +1,67 @@
 import "./sidebar.css";
+import React, {useState} from 'react';
 import Logo from "../../assets/Summertime White Element.png";
 import {Link} from 'react-router-dom'
 
 const Sidebar = () => {
+
+    const[toggle, showMenu] = useState(false);
+
     return(
-      <aside className='aside'>
-          <Link to="/" className="nav__logo">
-            <img src={Logo} alt="" />
-          </Link>
+        <>
+        <aside className={toggle ? "aside show-menu" : "aside"}>
+             <Link to="/" className="nav__logo">
+                <img src={Logo} alt="" />
+             </Link>
 
-          <nav className="nav">
-              <div className="nav__menu">
-                  <ul className="nav__list">
-                      <li className="nav__item">
-                          <div className="nav__link">
-                              <i className='icon-home'>
-                                <div className="icon_text">
-                                    <Link to="/" className="page__links">Home</Link>
-                                </div>
-                              </i>
+            <nav className="nav">
+                <div className="nav__menu">
+                    <ul className="nav__list">
+                        <li className="nav__item">
+                            <div className="nav__link">
+                                <i className='icon-home'>
+                                    <div className="icon_text">
+                                        <Link to="/" className="page__links">Home</Link>
+                                    </div>
+                                </i>
+                            </div>
+                        </li>
+
+                        <li className="nav__item">
+                            <div href="" className="nav__link">
+                                  <i className="icon-camrecorder">
+                                    <div className="icon_text">
+                                        <Link to="/video" className="page__links">Video</Link>
+                                    </div>
+                                </i>
                           </div>
-                      </li>
+                        </li>
 
-                      <li className="nav__item">
-                          <div href="" className="nav__link">
-                              <i className="icon-camrecorder">
-                                <div className="icon_text">
-                                <Link to="/video" className="page__links">Video</Link>
-                                </div>
-                              </i>
-                          </div>
-                      </li>
+                        <li className="nav__item">
+                            <div href="" className="nav__link">
+                                <i className="icon-bubble">
+                                    <div className="icon_text">
+                                        <Link to="/contact" className="page__links">Contact</Link>
+                                    </div>
+                                </i>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
 
-                      <li className="nav__item">
-                          <div href="" className="nav__link">
-                              <i className="icon-bubble">
-                                <div className="icon_text">
-                                    <Link to="/contact" className="page__links">Contact</Link>
-                                </div>
-                              </i>
-                          </div>
-                      </li>
-                  </ul>
-              </div>
-          </nav>
+            <div className="nav__footer">
+                <span className="copyright">
+                      &copy; 2024.
+                </span>
+            </div>
+        </aside>
 
-          <div className="nav__footer">
-              <span className="copyright">
-                  &copy; 2024.
-              </span>
-          </div>
-      </aside>
+        <div className={toggle ? "nav__toggle nav__toggle-open" : 
+            "nav__toggle"} onClick={() => showMenu(!toggle)}>
+            <i className="icon-menu"></i>
+        </div>
+        </>
     )
 }
 
